0583b7d61b35fdaf125228ba0753e711c07ed49a
lucascorreaa <lucas.lca14@gmail.com>
2024-12-04 14:51:24 -0300

    feat: added reactotron library

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ import { AppRegistry } from 'react-native'
 import { name as appName } from './app.json'
 import App from './src/App'
 
+if (__DEV__) {
+    require("./ReactotronConfig");
+  }
+
 AppRegistry.registerComponent(appName, () => App)
