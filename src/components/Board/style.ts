1bc12720c203ad390038f8a9b1f76f324a9e6213
henriquepablo <pablo269704@gmail.com>
2024-12-07 20:15:29 -0300

    create onboarding first screen of the code trouble

---
+++ src/components/Board/style.ts
@@ -0,0 +1,31 @@
+import { theme } from '@theme/theme'
+import styled from 'styled-components/native'
+
+export const Container = styled.View`
+    flex: 1;
+`
+
+export const ContainerImagem = styled.View<{ $large: boolean }>`
+    margin-top: ${(props) => (props.$large ? 48 : 19)}px;
+    margin-bottom: 10px;
+`
+
+export const Imagem = styled.Image`
+`
+
+export const Title = styled.Text<{ $large: boolean }>`
+    font-size: ${(props) => (props.$large ? 24 : 16)}px;
+    color: #323538;
+    font-family: ${theme.fonts.montserrat.semiBold};
+    margin-bottom: ${(props) => (props.$large ? 14 : 9)}px;
+    margin-left: 30px;
+    margin-right: 30px;
+`
+
+export const Text = styled.Text<{ $large: boolean }>`
+    font-size: ${(props) => (props.$large ? 20 : 14)}px;
+    font-family: ${theme.fonts.hind.light};
+    color: ${theme.colors.greyScale.dimGray};
+    margin-left: 30px;
+    margin-right: 30px;
+`
---
+++ src/screens/boarding/Boarding.tsx
@@ -0,0 +1,94 @@
+import RenderBoard from '@components/Board/RenderBoard'
+import { CompactLogo, NextButton } from '@icons/index'
+import { theme } from '@theme/theme'
+import breakpoints from '@utils/dimensons'
+import { useEffect, useRef, useState } from 'react'
+import AppIntroSlider from 'react-native-app-intro-slider'
+import { Container, ContainerLogo, Done, Skip, TextDone } from './style'
+
+export const Boarding: React.FC = () => {
+	const [color, setColor] = useState<string>(theme.colors.primary)
+	const [large, setLarge] = useState<boolean>(false)
+	const sliderRef = useRef<AppIntroSlider>(null)
+
+	useEffect(() => {
+		if (breakpoints() > 568) setLarge(true)
+	}, [])
+
+	const slides = [
+		{
+			key: '1',
+			title: 'Bem vindo a Code Trouble.',
+			text: 'Uma comunidade para colaborar e compartilhar, de devs para devs',
+			image: large
+				? require('../../assets/images/board1.png')
+				: require('../../assets/images/smallBoard1.png'),
+			color: `${theme.colors.primary}`,
+		},
+		{
+			key: '2',
+			title: 'Faça perguntas, encontre Respostas',
+			text: 'Nossa rede é focada em ajudar, aprender e ensinar.',
+			image: large
+				? require('../../assets/images/board2.png')
+				: require('../../assets/images/smallBoard2.png'),
+			color: `${theme.colors.accent}`,
+		},
+		{
+			key: '3',
+			title: 'Leia, escreva e compartilhe posts do nosso Blog!',
+			text: 'Escreva sobre o que te inspira e divulgue na nossa plataforma.',
+			image: large
+				? require('../../assets/images/board3.png')
+				: require('../../assets/images/smallBoard3.png'),
+			color: `${theme.colors.secondary}`,
+		},
+	]
+
+	const handleSlideChange = (index: number) => {
+		setColor(slides[index].color)
+	}
+
+	const handleSkip = () => {
+		const lastSlideIndex = slides.length - 1
+		setColor(slides[lastSlideIndex].color)
+		sliderRef.current?.goToSlide(lastSlideIndex)
+	}
+
+	return (
+		<Container>
+			<ContainerLogo $large={large}>
+				<CompactLogo color={color} width={large ? 60 : 36} />
+			</ContainerLogo>
+			<AppIntroSlider
+				data={slides}
+				ref={sliderRef}
+				renderItem={({ item }) => (
+					<RenderBoard image={item.image} text={item.text} title={item.title} />
+				)}
+				activeDotStyle={{
+					backgroundColor: color,
+					width: large ? 27 : 20,
+				}}
+				renderNextButton={() => (
+					<NextButton color={color} width={large ? 44 : 30} />
+				)}
+				showSkipButton={true}
+				renderSkipButton={() => (
+					<Skip $color={color} onPress={handleSkip} $large={large}>
+						Pular
+					</Skip>
+				)}
+				onSlideChange={handleSlideChange}
+				renderDoneButton={() => (
+					<Done $large={large}>
+						<TextDone $large={large}>Entrar e Começar</TextDone>
+					</Done>
+				)}
+				onDone={() => console.log('finalizado')}
+			/>
+		</Container>
+	)
+}
+
+export default Boarding
