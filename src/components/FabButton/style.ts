2ef463683f348fb28d2205e8a2d18cc2115f21cd
henriquepablo <pablo269704@gmail.com>
2025-01-01 19:38:35 -0300

    feat: adaptive component small screens

--- src/components/FabButton/style.ts
+++ src/components/FabButton/style.ts
@@ -2,16 +2,13 @@ import { theme } from '@theme/theme'
 import { Animated } from 'react-native'
 import styled from 'styled-components/native'
 
-export const Container = styled.View`
-    /* flex: 1; */
-`
-
 export const ContainerFabButton = styled(Animated.View)<{
 	$backgroundColor: string
 	$ml?: number
+	$large: boolean
 }>`
-    width: 54px;
-    height: 54px;
+    width: ${(props) => (props.$large ? 54 : 44)}px;
+    height: ${(props) => (props.$large ? 54 : 44)}px;
     border-radius: 100px;
     background-color: ${(props) => props.$backgroundColor};
     justify-content: center;
@@ -23,9 +20,10 @@ export const ContainerFabButton = styled(Animated.View)<{
 export const ContainerPlusButton = styled(Animated.View)<{
 	$backgroundColor: string
 	$ml?: number
+	$large: boolean
 }>`
-    width: 54px;
-    height: 54px;
+    width: ${(props) => (props.$large ? 54 : 44)}px;
+    height: ${(props) => (props.$large ? 54 : 44)}px;
     border-radius: 100px;
     background-color: ${(props) => props.$backgroundColor};
     justify-content: center;
@@ -37,11 +35,11 @@ export const ContainerPlusButton = styled(Animated.View)<{
     right: 20px;
 `
 
-export const Text = styled(Animated.Text)`
+export const Text = styled(Animated.Text)<{ $large: boolean }>`
     color: ${theme.colors.greyScale.offWhite};
     font-family: ${theme.fonts.hind.regular};
     width: 120px;
-    font-size: 15px;
+    font-size: ${(props) => (props.$large ? 15 : 14)}px;
 `
 
 export const Horizontal = styled.View`
