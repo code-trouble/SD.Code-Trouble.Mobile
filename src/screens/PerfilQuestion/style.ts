765303661b45ce094ebac9c8f9e227c425ca8158
henriquepablo <pablo269704@gmail.com>
2025-01-18 16:58:40 -0300

    feat: create screen person with animate, but not finalizate

---
+++ src/screens/PerfilQuestion/style.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native'
+import { theme } from '../../types/theme'
+
+export const Container = styled.View`
+    flex: 1;
+    padding-top: 70px;
+    background-color: ${theme.colors.greyScale.offWhite};
+`
